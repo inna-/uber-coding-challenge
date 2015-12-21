fe91084ef042d1c2e28fee984fda8edb5dbbe61c
Inna Kim <inneekim@gmail.com>
2015-12-21 03:20:40 -0500

    now shows only nearby places when a point on map clicked

--- food/views.py
+++ food/views.py
@@ -1,20 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.contrib.gis import geos
+from django.contrib.gis.geos import GEOSGeometry
+from django.contrib.gis.measure import D
 from models import Truck
 
 # Create your views here.
 
-def getTrucks(longitude, latitude):
-    pass
+def getTrucks(latitude, longitude):
+    current_point = GEOSGeometry('POINT(%s %s)' % (latitude, longitude), srid=4326)
+    trucks = Truck.objects.filter(latlon__distance_lte=(current_point, D(km=1)))
+    return trucks
 
 def index(request):
-    qs = Truck.objects.all()[:5]
-    for q in qs:
-        hours = q.schedule_set.all()
-        menus = q.menu_set.all()
+    try:
+        lat  = request.GET.get('lat')
+        lon  = request.GET.get('lon')
+        z = request.GET.get('zoom')
+        qs = getTrucks(lat, lon) 
+    except:
+        lat = 37.773972
+        lon = -122.431297
+        z = 12
+        qs = Truck.objects.all()
     template = loader.get_template('food/index.html')
-    context = RequestContext(request, {'qs': qs, 'hours': hours, 'menus': menus})
+    context = RequestContext(request, {'qs': qs, 'z': z})
 
     return HttpResponse(template.render(context))
 
